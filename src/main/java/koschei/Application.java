4470f82feac9108a9834f9e1c80965de7e8d32dd
VasmanT <vasman@mail.ru>
2024-12-05 10:39:44 +0300

    Add files via upload

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+                new AnnotationConfigApplicationContext(AppConfig.class);// создание коробки
         KoscheiTheDeathless koscheiTheDeathless =
-                applicationContext.getBean(KoscheiTheDeathless.class);
-        System.out.println(koscheiTheDeathless.getRulesByDeth());
+                applicationContext.getBean(KoscheiTheDeathless.class);  // достаем кощея из коробки
+        System.out.println(koscheiTheDeathless.getRulesByDeth()); // выводим в консоль строчку от Кощея
     }
 }
